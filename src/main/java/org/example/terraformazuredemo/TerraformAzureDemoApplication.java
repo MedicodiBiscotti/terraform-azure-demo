5e5e703cadfecca0a4fe39b0f33b39de02ad3014
MedicodiBiscotti <13235260+MedicodiBiscotti@users.noreply.github.com>
2024-01-13 23:46:21 +0100

    init

---
+++ src/main/java/org/example/terraformazuredemo/TerraformAzureDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.terraformazuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraformAzureDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TerraformAzureDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/terraformazuredemo/TerraformAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.terraformazuredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraformAzureDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
